564ba9a0038845fa90b43963d24ec8e38f56ea24
Emil Gigov <emilgigov1@gmail.com>
2026-07-16 11:22:53 +0300

    Bump to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "bg.tochka.reader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
